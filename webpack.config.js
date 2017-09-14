078871a9629b7c073f3f742ab824b3b24be5de78
Gustavo Morales <gustavo.morales@gmail.com>
2017-09-14 06:38:58 -0500

    Add print styles and update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,23 @@
 var path = require('path');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = [ {
     context: __dirname,
     entry: {
-        styles: [
-            './src/scss/main.scss'
-        ]
+        main: './src/scss/main.scss',
+        print: './src/scss/print.scss'
     },
     output: {
-        filename: 'main.css',
+        filename: '[name].css',
         path: './public/css/'
     },
     module: {
         loaders: [{
             test: /\.scss$/,
-            loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
         }]
     },
     plugins: [
-        new ExtractTextPlugin("main.css")
+        new ExtractTextPlugin('[name].css')
     ]
 }];
